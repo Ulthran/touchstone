1a5769b9926db3a1104a3008687ee07a2110039f
Ulthran <ctbushman@gmail.com>
2025-06-11 16:59:25 -0400

    Clean house

--- src/ui-components/index.js
+++
@@ -1,9 +0,0 @@
-/***************************************************************************
- * The contents of this file were generated with Amplify Studio.           *
- * Please refrain from making any modifications to this file.              *
- * Any changes to this file will be overwritten when running amplify pull. *
- **************************************************************************/
-
-export { default as JournalEntryCreateForm } from "./JournalEntryCreateForm";
-export { default as JournalEntryUpdateForm } from "./JournalEntryUpdateForm";
-export { default as studioTheme } from "./studioTheme";
